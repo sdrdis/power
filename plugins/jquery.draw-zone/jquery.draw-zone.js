445749f0db28bdd841b2b72d95d5c3e800b0ce68
Sébastien Drouyer <sdrdis@gmail.com>
2012-11-24 19:24:11 +0100

    added gettext prototype + added arrow

--- plugins/jquery.draw-zone/jquery.draw-zone.js
+++ plugins/jquery.draw-zone/jquery.draw-zone.js
@@ -32,6 +32,37 @@ $.widget("ui.drawZone", {
     this.context.stroke();
   },
 
+  drawArrow: function(x1, y1, x2, y2, width, color) {
+    var angleArrow = 40 * Math.PI / 180;
+    var lengthArrow = 15;
+
+    var angle = Math.atan2(y2 - y1, x2 - x1) + Math.PI;
+    var angle1 = angle + angleArrow;
+    var angle2 = angle - angleArrow;
+
+    var xa1 = Math.cos(angle1) * lengthArrow + x2;
+    var ya1 = Math.sin(angle1) * lengthArrow + y2;
+
+    var xa2 = Math.cos(angle2) * lengthArrow + x2;
+    var ya2 = Math.sin(angle2) * lengthArrow + y2;
+
+    this.drawOutline(x1, y1, x2, y2, width);
+    this.drawOutline(x2, y2, xa1, ya1, width);
+    this.drawOutline(x2, y2, xa2, ya2, width);
+    this.drawLine(x1, y1, x2, y2, width, color);
+    this.drawLine(x2, y2, xa1, ya1, width, color);
+    this.drawLine(x2, y2, xa2, ya2, width, color);
+  },
+
+  drawOutline: function(x1, y1, x2, y2, width) {
+    var diffX = x2 - x1;
+    var diffY = y2 - y1;
+    var length = Math.sqrt(diffX * diffX + diffY * diffY);
+    var unitX = diffX / length;
+    var unitY = diffY / length;
+    this.drawLine(x1 - unitX * 2, y1 - unitY * 2, x1 + unitX * (length + 2), y1 + unitY * (length + 2), width + 4, 'black');
+  },
+
   drawCircle: function(x, y, radius, width, color, type) {
     if (typeof type == 'undefined') {
       type = 'stroke';
