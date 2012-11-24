6b74eefb2750c240282424d85751f463e396b286
Adam <adamrse@gmail.com>
2012-11-24 17:38:36 +0100

    methodes can de player

--- js/class/player.js
+++ js/class/player.js
@@ -5,9 +5,41 @@ Player = new Class({
         this.hq = hq;
         this.units = [];
     },
-    createUnit: function(name, position) {
+    createUnit: function(name, position) {//position 
         var unit = new window[name](position || this.hq);
         unit.player = this;
         this.units.push(unit);
+    },
+    canMove: function(unit, position) {
+        return unit.canMove(position);
+    },
+    canFusion: function(unit) {
+        var unitsFusion = Game.getUnitsOnCell(unit.position);
+        
+        var permit = false;
+        var nbFusion = 0;
+        
+        unitsFusion.forEach(function(unitFusion){
+            if(unitFusion.type == unit.type){ nbFusion++; }//compte les unités de même type
+        });
+        if(nbFusion >= 3){ permit = true }//si au moins 3 unités du même type, on peux fusionner
+        
+        return permit;
+    },
+    canMegaMissile: function(unit) {
+        var unitsFusion = Game.getUnitsOnCell(unit.position);
+        
+        var permit = false;
+        var countPower = 0;
+        
+        unitsFusion.forEach(function(unitFusion){
+            countPower += unitFusion.power
+        });
+        if(countPower >= 100){ permit = true }//si au moins 3 unités du même type, on peux fusionner
+        
+        return permit;
+    },
+    canBuy: function(type) {
+        
     }
 });
